7a50a74cb95b85db4a81975b6a5d3ba6c13df23e
devin hanaway <devinh11@gmail.com>
2017-07-13 18:09:09 -0600

    decoupled this little app

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-export default()=>{
-  mongoose.Promise = global.Promise
-  mongoose.connect('mongodb://localhost/nomads')
-  mongoose.connection
-    .once('open', ()=> console.log('mongoose is running'))
-    .on('error', err => console.error(err))
-}
